d5cdeffad728ba41884976352b670d5c231cb249
Abdelrahman-Abdelaziim <149614405+Abdelrahman-Abdelaziim@users.noreply.github.com>
2025-06-21 14:17:03 +0300

    refactor

--- src/main/java/com/swagLabs/utils/ScreenshotsUtils.java
+++ src/main/java/com/swagLabs/utils/ScreenshotsUtils.java
@@ -1,11 +1,8 @@
 package com.swagLabs.utils;
-
-import com.swagLabs.drivers.GUIDriver;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import java.io.File;
 
 import static com.swagLabs.utils.TimestampUtils.getTimestamp;
